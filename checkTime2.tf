4acd008e837be95404f0f80d38d69ebfa3a565f8
bc-owner-auto <110401622+bc-owner-auto@users.noreply.github.com>
2022-12-06 11:06:52 +0200

    Create checkTime2.tf

---
+++ checkTime2.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_function" "analysis_lambda" {
+  # lambda have plain text secrets in environment variables
+  filename      = "resources/lambda_function_payload.zip"
+  function_name = "${local.resource_prefix.value}-analysis"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+}
